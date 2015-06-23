754a128b6ddbb1039a06ded87a9d07dace2dedbe
Sunjay Varma <varma.sunjay@gmail.com>
2015-06-23 18:27:08 -0400

    Made panel formatting work

--- panel-view.js
+++ panel-view.js
@@ -23,22 +23,56 @@ export default class PanelView extends blessed.Box {
 
   renderPanels() {
     const lightArray = this.store.data.get('lights');
-    const output = [for (stripId of lightArray) this.formatStrip(lightArray.get(stripId))];
-    this.setContent(output.join('\n'));
+    const formattedStrips = Array.from(this.formatStrips(lightArray));
+    this.setContent(formattedStrips.join('\n'));
   }
 
-  formatStrip(strip) {
-    const formattedPanels = [];
+  *formatStrips(lightArray) {
+    for (let stripId of lightArray.stripIds) {
+      const formattedPanels = Array.from(this.formatStripPanels(lightArray, stripId));
+      yield `${stripId}: ${formattedPanels.join(' ')}`;
+    }
+  }
+
+  *formatStripPanels(lightArray, stripId) {
+    const strip = lightArray.get(stripId);
     for (let panelId of strip.panelIds) {
-      const panel = strip.get(panelId);
-      const formattedPanel = this.formatPanel(panel);
-      formattedPanels.push(formattedPanel);
+      yield this.formatPanel(lightArray, stripId, panelId);
+    }
+  }
+
+  formatPanel(lightArray, stripId, panelId) {
+    const panelIntensity = lightArray.getIntensity(stripId, panelId);
+
+    const cellWidth = 3;
+    // pad cell to cellWidth and right align in cell
+    let formattedPanel = ((' '.repeat(cellWidth)) + panelIntensity).slice(-cellWidth);
+
+    if (panelIntensity > 50) {
+      formattedPanel = `{bold}${formattedPanel}{/bold}`;
     }
 
-    return formattedPanels.join(' ');
+    const panelColor = this._colorFromKeyword(lightArray.getColor(stripId, panelId));
+    if (panelColor) {
+      formattedPanel = `{${panelColor}-fg}${formattedPanel}{/${panelColor}-fg}`;
+    }
+
+    const panelActive = lightArray.isActive(stripId, panelId);
+    if (panelActive) {
+      formattedPanel = `{white-bg}${formattedPanel}{/white-bg}`;
+    }
+
+    return formattedPanel;
   }
 
-  formatPanel(panel) {
-    return `${panel.get('intensity')}`;
+  _colorFromKeyword(keyword) {
+    switch (keyword) {
+      case "success":
+        return "green";
+      case "error":
+        return "red";
+      default:
+        return;
+    }
   }
 }
