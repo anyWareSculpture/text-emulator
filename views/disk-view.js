54fd9c433c809b0490bd164fcb7e7e2588c62a33
Sunjay Varma <varma.sunjay@gmail.com>
2015-07-30 00:19:13 -0400

    Made it so that the disk view turns the disks when their direction indicates so

--- views/disk-view.js
+++ views/disk-view.js
@@ -1,11 +1,13 @@
 const blessed = require('blessed');
 
-const {SculptureStore} = require('@anyware/game-logic');
+const {SculptureStore, DisksActionCreator, Disk} = require('@anyware/game-logic');
 
 const VIEW_TITLE = "{center}{bold}Disks{/bold}{/center}";
 const CELL_WIDTH = 3;
+const DISK_TURN_UPDATE_INTERVAL = 500; // ms
+const DISK_MAX_POSITION = 30;
 
-export default class PanelView extends blessed.Box {
+export default class DiskView extends blessed.Box {
   constructor(store, dispatcher, windowOptions) {
     super(Object.assign({
       tags: true,
@@ -21,9 +23,12 @@ export default class PanelView extends blessed.Box {
     this.store = store;
 
     this._animating = false;
+    this._disksActionCreator = new DisksActionCreator(dispatcher);
 
     this.renderDisks();
     this.store.on(SculptureStore.EVENT_CHANGE, this._handleChanges.bind(this));
+
+    setInterval(this._updateDiskPositions.bind(this), DISK_TURN_UPDATE_INTERVAL);
   }
 
   renderDisks() {
@@ -63,8 +68,20 @@ export default class PanelView extends blessed.Box {
 
   formatDisk(diskId, disk) {
     let formattedDisk =`{yellow-fg}${diskId}:{/yellow-fg}  `;
-    formattedDisk += `{yellow-fg}POS:{/yellow-fg} ${disk.getPosition()} `;
-    formattedDisk += `{yellow-fg}DIR:{/yellow-fg} ${disk.getDirection()} `;
+
+    const formattedPos = (" " + disk.getPosition()).slice(-2);
+    formattedDisk += `{yellow-fg}POS:{/yellow-fg} ${formattedPos} `;
+
+    let formattedDir = {
+      [Disk.CLOCKWISE]: "+1",
+      [Disk.COUNTERCLOCKWISE]: "-1",
+      [Disk.STOPPED]: "0",
+      [Disk.CONFLICT]: "x"
+    }[disk.getDirection()];
+    formattedDir = (" " + formattedDir).slice(-2);
+
+    formattedDisk += `{yellow-fg}DIR:{/yellow-fg} ${formattedDir} `;
+
     formattedDisk += `{yellow-fg}STATE:{/yellow-fg} ${disk.getState()} `;
     formattedDisk += `{yellow-fg}USER:{/yellow-fg} '${disk.getUser()}'`;
     return formattedDisk;
@@ -84,4 +101,35 @@ export default class PanelView extends blessed.Box {
       return;
     }
   }
+
+  _updateDiskPositions() {
+    const disks = this.store.data.get('disks');
+    
+    for (let diskId of disks) {
+      const disk = disks.get(diskId);
+      let position = disk.getPosition();
+
+      if (disk.isTurningClockwise) {
+        position += 1;
+      }
+      else if (disk.isTurningCounterclockwise) {
+        position -= 1;
+      }
+      else {
+        continue;
+      }
+
+      if (position < 0) {
+        position += DISK_MAX_POSITION;
+      }
+      else {
+        position %= DISK_MAX_POSITION;
+      }
+
+      this._disksActionCreator.sendDiskUpdate(diskId, {
+        position: position
+      });
+    }
+  }
 }
+
