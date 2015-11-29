0beeb91889bbc27b9625a3e7bd9f04a3f184d322
Sunjay Varma <varma.sunjay@gmail.com>
2015-11-29 16:23:54 -0500

    Script for generating credentials file

--- scripts/generate-credentials.js
+++ scripts/generate-credentials.js
@@ -15,6 +15,11 @@ console.log("Press Ctrl+C to cancel and quit at any point".red);
 console.log("".yellow);
 
 generateCredentialsContent().then((content) => {
+  if (!Object.keys(content).length) {
+    console.log('No content to write...aborting.'.red);
+    return Promise.resolve();
+  }
+
   return promptValue('\nOutput Filename: ', '.credentials').then((filename) => {
     return new Promise(function(resolve, reject) {
       const stringContents = JSON.stringify(content, null, 2);
@@ -42,35 +47,80 @@ function generateCredentialsContent() {
         return Promise.resolve(content);
       }
       Object.assign(content, environmentSection);
-      return getEnvironmentSection().then(appendEnvironmentSection);
+
+      const defaultHost = environmentSection[Object.keys(environmentSection)[0]].host;
+      return getEnvironmentSection(defaultHost).then(appendEnvironmentSection);
     };
 
     getEnvironmentSection().then(appendEnvironmentSection).then(resolve).catch(reject);
   });
 }
 
-function getEnvironmentSection() {
+function getEnvironmentSection(defaultHost='') {
   console.log("\nEnter 'q' when you are done.");
-  return promptValues(['environment name: ', 'host: ']).then(([env, host]) => {
-    if (env === 'q' || host === 'q') {
+  return promptValues([
+    'environment host: ', 'alias: '
+  ], [
+    defaultHost, 'dev'
+  ]).then(([host, env]) => {
+    if (!env || !host) {
       return Promise.resolve({});
     }
 
-    const section = {
-      [env]: {
-        host: host,
-        users: {}
-      }
-    };
-
-    return Promise.resolve(section);
+    return getEnvironmentUsers().then((users) => {
+      return Promise.resolve({
+        [env]: {
+          host: host,
+          users: users
+        }
+      });
+    });
   });
 }
 
-function promptValues(descriptions, defaultValue='') {
+function getEnvironmentUsers() {
+  console.log("\nEnter 'q' when you are done.");
+  return new Promise((resolve, reject) => {
+    let index = 0;
+    const content = {};
+    const append = (creds) => {
+      if (!Object.keys(creds).length) {
+        return Promise.resolve(content);
+      }
+      const {username, password} = creds;
+      content[username] = password;
+
+      return getUser(++index).then(append);
+    };
+
+    getUser(++index).then(append).then(resolve).catch(reject);
+  });
+}
+
+function getUser(number) {
+  console.log(`Credentials for user ${number}:`);
+  return promptValues(['username: ', 'password: ']).then(([username, password]) => {
+    if (!username || !password) {
+      return Promise.resolve({});
+    }
+
+    return Promise.resolve({
+      username: username,
+      password: password
+    });
+  });
+}
+
+function promptValues(descriptions, defaultValues='') {
   const accumulator = [];
   const promptDescription = (index) => {
+    const defaultValue = defaultValues instanceof Array ? defaultValues[index] : defaultValues;
     return promptValue(descriptions[index], defaultValue).then((value) => {
+      if (value.trim() === 'q') {
+        // Discard all input and return empty
+        return Array(descriptions.length).join(".").split(".");
+      }
+
       accumulator.push(value);
 
       index++;
@@ -90,7 +140,8 @@ function promptValue(description, defaultValue='') {
       properties: {
         value: {
           description: description.cyan,
-          default: defaultValue
+          default: defaultValue,
+          required: true
         }
       }
     }, (err, result) => {
