e181b5a8f15decd085720616a322a4ce4e77bb47
Sunjay Varma <varma.sunjay@gmail.com>
2015-11-29 17:27:41 -0500

    Implemented support for credentials file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('source-map-support').install();
 
+const fs = require('fs');
+
 const Config = require('./config');
 const EmulatorApp = require('./application');
 
@@ -20,7 +22,7 @@ process.on('uncaughtException', function(err) {
     stack = err.toString();
   }
 
-  require('fs').appendFileSync('error.log', `[${currentDay} ${currentTime}] ${stack}\n`);
+  fs.appendFileSync('error.log', `[${currentDay} ${currentTime}] ${stack}\n`);
 
   if (app) {
     // Not a best practice
@@ -38,13 +40,68 @@ const config = new Config();
 app = new EmulatorApp(config);
 
 const connectionOptions = Object.assign({}, config.CLIENT_CONNECTION_OPTIONS.default);
-if (process.argv.length === 4) {
-  console.log("Using authentication information provided by command arguments");
-  config.username = process.argv[2];
-  connectionOptions.username = process.argv[2];
-  connectionOptions.password = process.argv[3];
+if (process.argv.length > 2) {
+  const args = process.argv.slice(2);
+  
+  parseArgs(args, connectionOptions);
+  // Hack because shiftr doesn't support multiple same usernames across
+  // namespaces and we need the usernames to be 'sculpture0', etc.
+  config.username = connectionOptions.username.split('-')[0];
 }
 
 app.connectAndSetup(connectionOptions);
 
 app.render();
+
+function parseArgs(args, options) {
+  // Expects at least 1 argument in args
+  // Allows for:
+  // * Password from environment and username:
+  //   <env alias> <username>
+  // * Any username from environment:
+  //   <env alias>
+  // * Host from environment:
+  //   <env alias> <username> <password>
+  // * Just credentials on default host:
+  //   - <username> <password>
+  // * Complete credentials:
+  //   <host> <username> <password>
+
+  let credentials;
+  try {
+    credentials = JSON.parse(fs.readFileSync('.credentials'));
+  }
+  catch (e) {
+    console.warn("Warning: No parsable .credentials file found, trying reasonable defaults");
+    credentials = {};
+  }
+
+
+  let [env, username, password] = args;
+  let host = options.host;
+  if (env !== '-') {
+    console.log("Using authentication information provided by .credentials");
+    if (credentials.hasOwnProperty(env)) {
+      const envInfo = credentials[env];
+      host = envInfo.host;
+      username = username || Object.keys(envInfo.users)[0];
+      password = password || envInfo.users[username];
+    }
+    else {
+      console.log("Assuming given environment argument is host");
+      host = env;
+    }
+  }
+  else {
+    console.log("Using authentication information provided by command arguments");
+  }
+
+  if (username && password) {
+    options.host = host;
+    options.username = username;
+    options.password = password;
+  }
+  else {
+    console.error("Not enough information. Continuing with defaults.");
+  }
+}
